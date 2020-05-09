b309aa2346e6a13eb452e28a1fa4bfcb7896c107
Doron Horwitz <doron@at-bay.com>
2020-05-09 15:59:46 +0300

    auth

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
-from flask import Flask
+import os
+from flask import (
+    abort,
+    Flask,
+    render_template,
+    request,
+    send_from_directory,
+)
+from utils.auth import (
+    generate_github_app_token,
+    generate_github_app_installation_token,
+    verify_github_webhook_payload,
+)
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/github-event-handler')
+def github_event_handler():
+    # check the API request has been signed correctly
+    if not verify_github_webhook_payload(
+        os.environ['GITHUB_WEBHOOK_SECRET'],
+        request.headers['X-Hub-Signature'].split('=')[1],
+        request.data,
+    ):
+        abort(400)
+
+    github_app_token = generate_github_app_token(
+        int(os.environ['GITHUB_APP_IDENTIFIER']),
+        os.environ['GITHUB_PRIVATE_KEY'].encode(),
+    )
+
+    installation_id = request.json()['installation']['id']
+    github_app_installation_token = generate_github_app_installation_token(github_app_token, installation_id)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon',
+    )
 
 
 if __name__ == '__main__':
---
+++ utils/auth.py
@@ -0,0 +1,38 @@
+import hmac
+import requests
+import time
+from jwcrypto.jwt import JWT
+from jwcrypto.jwk import JWK
+
+
+def verify_github_webhook_payload(github_webhook_secret, signature, payload):
+
+    mac = hmac.new(github_webhook_secret, msg=payload, digestmod='sha1')
+    return hmac.compare_digest(mac.hexdigest(), signature)
+
+
+def generate_github_app_token(github_app_identifier, github_private_key):
+    now = int(time.time())
+    token = JWT(
+        header={'alg': 'RS256'},
+        claims={
+            'iat': now,
+            'exp': now + (10 * 60),
+            'iss': github_app_identifier,
+        },
+        algs=['RS256'],
+    )
+
+    token.make_signed_token(JWK.from_pem(github_private_key))
+    return token.serialize()
+
+
+def generate_github_app_installation_token(github_app_token, installation_id):
+    response = requests.post(
+        f'https://api.github.com/app/installations/{installation_id}/access_tokens',
+        headers={
+            'Authorization': f'Bearer {github_app_token}',
+            'Accept': 'application/vnd.github.machine-man-preview+json',
+        }
+    ).json()
+    return response['token']
