5f61ddea3314cca28ab5c702f130d408496b8a0c
Doron Horwitz <doron@at-bay.com>
2020-05-10 03:39:12 +0300

    access tokens

--- app.py
+++ app.py
@@ -1,27 +1,88 @@
 import json
 import os
+import requests
 from flask import (
     abort,
     Flask,
+    redirect,
     render_template,
     request,
     send_from_directory,
+    session,
+    url_for,
 )
 from utils.auth import (
     generate_github_app_token,
     get_github_app_installation_token,
-    verify_github_webhook_payload,
+    generate_secure_random_string,
+    verify_github_webhook_payload, get_access_token,
 )
 from utils.logging import configure as configure_logging
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']
 configure_logging(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    (session['access_token'],
+     session['access_token_expiry'],
+     session['refresh_token'],
+     session['refresh_token_expiry']) = get_access_token(
+        current_access_token=session.get('access_token'),
+        github_app_client_id=os.environ['GITHUB_APP_CLIENT_ID'],
+        github_app_client_secret=os.environ['GITHUB_APP_CLIENT_SECRET'],
+        access_token_expiry=session.get('access_token_expiry'),
+        refresh_token=session.get('refresh_token'),
+        refresh_token_expiry=session.get('refresh_token_expiry'),
+    )
+
+    if session.get('access_token') is None:
+        return redirect(url_for('signin'))
+
+    return render_template('index.html', access_token=session.get('access_token'))
+
+
+@app.route('/signin')
+def signin():
+
+    (session['access_token'],
+     session['access_token_expiry'],
+     session['refresh_token'],
+     session['refresh_token_expiry']) = get_access_token(
+        current_access_token=session.get('access_token'),
+        github_app_client_id=os.environ['GITHUB_APP_CLIENT_ID'],
+        github_app_client_secret=os.environ['GITHUB_APP_CLIENT_SECRET'],
+        access_token_expiry=session.get('access_token_expiry'),
+        refresh_token=session.get('refresh_token'),
+        refresh_token_expiry=session.get('refresh_token_expiry'),
+        sent_state=request.args.get('state'),
+        expected_state=session.get('state'),
+        code=request.args.get('code'),
+    )
+
+    if session['access_token']:
+        return redirect(url_for('index'))
+    else:
+        if session.get('state') is None:
+            session['state'] = generate_secure_random_string()
+        return render_template(
+            'signin.html',
+            client_id=os.environ['GITHUB_APP_CLIENT_ID'],
+            redirect_uri=url_for(request.endpoint, _external=True),
+            state=session['state'],
+        )
+
+
+@app.route('/signout')
+def signout():
+    session.pop('access_token', None)
+    session.pop('access_token_expiry', None)
+    session.pop('refresh_token', None)
+    session.pop('refresh_token_expiry', None)
+    return redirect(url_for('index'))
 
 
 @app.route('/github-event-handler', methods=['POST'])
@@ -31,15 +92,15 @@ def github_event_handler():
     private_key = os.environ['GITHUB_PRIVATE_KEY'].encode()
     _, _, signature = request.headers['X-Hub-Signature'].partition('=')
     event = request.headers['X-Github-Event']
+    delivery_id = request.headers['X-Github-Delivery']
 
     # check the API request has been signed correctly
     if not verify_github_webhook_payload(secret, signature, request.data):
-        abort(400)
+        return abort(400)
 
     github_app_token = generate_github_app_token(app_identifier, private_key)
     installation_id = request.json['installation']['id']
     github_app_installation_token = get_github_app_installation_token(github_app_token, installation_id)
-    app.logger.info(github_app_installation_token)
     app.logger.info(json.dumps(request.json, sort_keys=True, indent=4))
     return "hi"
 
--- utils/auth.py
+++ utils/auth.py
@@ -1,8 +1,17 @@
+import calendar
 import hmac
 import requests
 import time
+from base64 import b64encode
+from datetime import datetime
+from os import urandom
 from jwcrypto.jwt import JWT
 from jwcrypto.jwk import JWK
+
+
+def generate_secure_random_string():
+    random_bytes = urandom(64)
+    return b64encode(random_bytes).decode('utf-8')
 
 
 def verify_github_webhook_payload(github_webhook_secret, signature, payload):
@@ -38,3 +47,56 @@ def get_github_app_installation_token(github_app_token, installation_id):
         return response['token']
     except KeyError:
         return None
+
+
+def get_timestamp():
+    return calendar.timegm(datetime.utcnow().utctimetuple())
+
+
+def get_access_token(
+    current_access_token,
+    github_app_client_id,
+    github_app_client_secret,
+    access_token_expiry,
+    refresh_token,
+    refresh_token_expiry,
+    sent_state=None,
+    expected_state=None,
+    code=None,
+):
+
+    timestamp = get_timestamp()
+
+    refresh_buffer = 30
+    access_token_expired = access_token_expiry is None or access_token_expiry - timestamp < refresh_buffer
+    refresh_token_expired = refresh_token_expiry is None or refresh_token_expiry - timestamp < refresh_buffer
+
+    if current_access_token and not access_token_expired:
+        return current_access_token, access_token_expiry, refresh_token, refresh_token_expiry
+    elif refresh_token and not refresh_token_expired:
+        request_params = {
+            'refresh_token': refresh_token,
+            'grant_type': 'refresh_token',
+            'client_id': github_app_client_id,
+            'client_secret': github_app_client_secret,
+        }
+    elif code and sent_state and expected_state and sent_state == expected_state:
+        request_params = {
+            'client_id': github_app_client_id,
+            'client_secret': github_app_client_secret,
+            'code': code,
+        }
+    else:
+        return (None,) * 4
+
+    response = requests.post('https://github.com/login/oauth/access_token', params=request_params, headers={
+        'Accept': 'application/json'
+    }).json()
+
+    return (
+        response['access_token'],
+        timestamp + response['expires_in'],
+        response['refresh_token'],
+        timestamp + response['refresh_token_expires_in']
+    )
+ß
